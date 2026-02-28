5ab85602ecf9c3784e2d2c6758fb34e6c63f4b55
Philip Borenstein <pborenstein@gmail.com>
2026-02-27 22:27:51 -0500

    Set title to Pamphlet

--- content/_data/metadata.js
+++ content/_data/metadata.js
@@ -1,5 +1,5 @@
 export default {
-  title: "My Literary Work",
+  title: "Pamphlet",
   url: "https://example.com/",
   language: "en",
   description: "A description of this literary work",
--- content/index.md
+++ content/index.md
@@ -1,5 +1,5 @@
 ---
-title: My Literary Work
+title: Pamphlet
 layout: base.njk
 ---
 
