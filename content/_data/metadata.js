8422ba1f44b584a376a62ceeafdc74f8996ec00f
Philip Borenstein <pborenstein@gmail.com>
2026-02-27 22:36:15 -0500

    Set URL to orobia.lol, update about page

--- content/_data/metadata.js
+++ content/_data/metadata.js
@@ -1,12 +1,12 @@
 export default {
   title: "Pamphlet",
-  url: "https://example.com/",
+  url: "https://orobia.lol/",
   language: "en",
   description: "A description of this literary work",
   author: {
     name: "Your Name",
     email: "you@example.com",
-    url: "https://example.com/"
+    url: "https://orobia.lol/"
   },
   image: "/img/cover.png",
   twitter: "@yourhandle"
--- content/about.md
+++ content/about.md
@@ -5,6 +5,6 @@ layout: base.njk
 
 # [{{ title }}](/)
 
-This is where you write about yourself and your work.
+This is where you write about yourself and your work. This site is served from [orobia.lol](https://orobia.lol/).
 
 You can include links, images, and any other content that helps readers learn more about you.
